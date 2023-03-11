4f3241ef89f349cee94aa0892aa2d2e42d59c885
Bau Huynh <bauhuynhdev@gmail.com>
2023-03-11 22:16:42 +0700

    - Update install.sh

--- install.sh
+++ install.sh
@@ -10,94 +10,81 @@ install_common() {
 
 install_nginx() {
   # shellcheck disable=SC2046
-  if [ $(command -v nginx) ]; then
-    echo "----------------------------------- Nginx installed -----------------------------------"
-  else
+  if ! [ $(command -v nginx) ]; then
     echo "----------------------------------- Installing Nginx -----------------------------------"
     curl https://raw.githubusercontent.com/bauhuynhdev/linux-repo/master/nginx.repo -L -o /etc/yum.repos.d/nginx.repo
     yum install -y nginx
-    echo "----------------------------------- Nginx installed -----------------------------------"
   fi
+  echo "----------------------------------- Nginx installed -----------------------------------"
 }
 
 install_php() {
   # shellcheck disable=SC2046
-  if [ $(command -v php) ]; then
-    echo "----------------------------------- PHP installed -----------------------------------"
-  else
+  if ! [ $(command -v php) ]; then
     echo "----------------------------------- Installing PHP -----------------------------------"
     yum install -y https://rpms.remirepo.net/enterprise/remi-release-7.rpm
     yum --enablerepo=remi-php74 install -y php php-cli php-fpm php-zip php-devel php-gd php-mcrypt php-mbstring php-curl php-xml php-pear php-bcmath php-json php-mysql php-redis -y
-    echo "----------------------------------- PHP installed -----------------------------------"
   fi
+  echo "----------------------------------- PHP installed -----------------------------------"
 }
 
 install_composer() {
   # shellcheck disable=SC2046
-  if [ $(command -v composer) ]; then
-    echo "----------------------------------- Composer installed -----------------------------------"
-  else
+  if ! [ $(command -v composer) ]; then
     echo "----------------------------------- Installing Composer -----------------------------------"
     php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
     php composer-setup.php --install-dir=/usr/local/bin --filename=composer
     php -r "unlink('composer-setup.php');"
-    echo "----------------------------------- Composer installed -----------------------------------"
   fi
+  echo "----------------------------------- Composer installed -----------------------------------"
 }
 
 install_nodejs_npm_pm2() {
   # shellcheck disable=SC2046
-  if [ $(command -v node) ]; then
-    echo "----------------------------------- Nodejs, NPM and PM2 installed -----------------------------------"
-  else
+  if ! [ $(command -v node) ]; then
     echo "----------------------------------- Installing Nodejs, NPM and PM2 -----------------------------------"
     curl -sL https://rpm.nodesource.com/setup_14.x | bash -
     yum install -y nodejs
     npm install -g yarn pm2
-    echo "----------------------------------- Nodejs, NPM and PM2 installed -----------------------------------"
   fi
+  echo "----------------------------------- Nodejs, NPM and PM2 installed -----------------------------------"
 }
 
 install_postgres() {
   # shellcheck disable=SC2046
-  if [ $(command -v psql) ]; then
-    echo "----------------------------------- Postgres installed -----------------------------------"
-  else
+  if ! [ $(command -v psql) ]; then
     echo "----------------------------------- Installing Postgres -----------------------------------"
     yum install -y https://raw.githubusercontent.com/bauhuynhdev/linux-repo/master/pgdg-redhat-repo-latest.noarch.rpm
     yum install -y postgresql13-server
     /usr/pgsql-13/bin/postgresql-13-setup initdb
-    echo "----------------------------------- Postgres installed -----------------------------------"
   fi
+  echo "----------------------------------- Postgres installed -----------------------------------"
 }
 
 install_mysql() {
   # shellcheck disable=SC2046
-  if [ $(command -v mysql) ]; then
-    echo "----------------------------------- MySQL installed -----------------------------------"
-  else
+  if ! [ $(command -v mysql) ]; then
     echo "----------------------------------- Installing MySQL -----------------------------------"
     rpm -Uvh https://dev.mysql.com/get/mysql80-community-release-el7-7.noarch.rpm
     sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/mysql-community.repo
     yum --enablerepo=mysql80-community install mysql-community-server -y
-    echo "----------------------------------- MySQL installed -----------------------------------"
   fi
+  echo "----------------------------------- MySQL installed -----------------------------------"
 }
 
 install_redis() {
   # shellcheck disable=SC2046
-  if [ $(command -v redis-cli) ]; then
-    echo "----------------------------------- Redis installed -----------------------------------"
-  else
+  if ! [ $(command -v redis-cli) ]; then
     echo "----------------------------------- Installing Redis -----------------------------------"
     # Installed repo redis in PHP
     yum --enablerepo=remi install redis -y
-    echo "----------------------------------- Redis installed -----------------------------------"
   fi
+  echo "----------------------------------- Redis installed -----------------------------------"
 }
 
 enable_and_start_services() {
   yum update -y
+  echo "----------------------------------- Enabling and starting services -----------------------------------"
   # shellcheck disable=SC2046
   if [ $(command -v systemctl) ]; then
 
