fc419766285019452f426626c66b3f731bc010bf
Bau Huynh <bauhuynhdev@gmail.com>
2023-03-11 22:23:41 +0700

    - Update install.sh

--- install.sh
+++ install.sh
@@ -1,108 +1,114 @@
 #!/bin/bash
 
 install_common() {
-  echo "----------------------------------- Installing Common -----------------------------------"
+  echo "*********************************** Installing Common ***********************************"
   yum update -y
   yum install -y epel-release yum-utils zip unzip net-tools curl wget gcc-c++ make nano openssl htop
   yum update -y
-  echo "----------------------------------- Common installed -----------------------------------"
+  echo "*********************************** Common installed ***********************************"
 }
 
 install_nginx() {
   # shellcheck disable=SC2046
   if ! [ $(command -v nginx) ]; then
-    echo "----------------------------------- Installing Nginx -----------------------------------"
+    echo "*********************************** Installing Nginx ***********************************"
     curl https://raw.githubusercontent.com/bauhuynhdev/linux-repo/master/nginx.repo -L -o /etc/yum.repos.d/nginx.repo
     yum install -y nginx
   fi
-  echo "----------------------------------- Nginx installed -----------------------------------"
+  echo "*********************************** Nginx installed ***********************************"
 }
 
 install_php() {
   # shellcheck disable=SC2046
   if ! [ $(command -v php) ]; then
-    echo "----------------------------------- Installing PHP -----------------------------------"
+    echo "*********************************** Installing PHP ***********************************"
     yum install -y https://rpms.remirepo.net/enterprise/remi-release-7.rpm
     yum --enablerepo=remi-php74 install -y php php-cli php-fpm php-zip php-devel php-gd php-mcrypt php-mbstring php-curl php-xml php-pear php-bcmath php-json php-mysql php-redis -y
   fi
-  echo "----------------------------------- PHP installed -----------------------------------"
+  echo "*********************************** PHP installed ***********************************"
 }
 
 install_composer() {
   # shellcheck disable=SC2046
   if ! [ $(command -v composer) ]; then
-    echo "----------------------------------- Installing Composer -----------------------------------"
+    echo "*********************************** Installing Composer ***********************************"
     php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
     php composer-setup.php --install-dir=/usr/local/bin --filename=composer
     php -r "unlink('composer-setup.php');"
   fi
-  echo "----------------------------------- Composer installed -----------------------------------"
+  echo "*********************************** Composer installed ***********************************"
 }
 
 install_nodejs_npm_pm2() {
   # shellcheck disable=SC2046
   if ! [ $(command -v node) ]; then
-    echo "----------------------------------- Installing Nodejs, NPM and PM2 -----------------------------------"
+    echo "*********************************** Installing Nodejs, NPM and PM2 ***********************************"
     curl -sL https://rpm.nodesource.com/setup_14.x | bash -
     yum install -y nodejs
     npm install -g yarn pm2
   fi
-  echo "----------------------------------- Nodejs, NPM and PM2 installed -----------------------------------"
+  echo "*********************************** Nodejs, NPM and PM2 installed ***********************************"
 }
 
 install_postgres() {
   # shellcheck disable=SC2046
   if ! [ $(command -v psql) ]; then
-    echo "----------------------------------- Installing Postgres -----------------------------------"
+    echo "*********************************** Installing Postgres ***********************************"
     yum install -y https://raw.githubusercontent.com/bauhuynhdev/linux-repo/master/pgdg-redhat-repo-latest.noarch.rpm
     yum install -y postgresql13-server
     /usr/pgsql-13/bin/postgresql-13-setup initdb
   fi
-  echo "----------------------------------- Postgres installed -----------------------------------"
+  echo "*********************************** Postgres installed ***********************************"
 }
 
 install_mysql() {
   # shellcheck disable=SC2046
   if ! [ $(command -v mysql) ]; then
-    echo "----------------------------------- Installing MySQL -----------------------------------"
+    echo "*********************************** Installing MySQL ***********************************"
     rpm -Uvh https://dev.mysql.com/get/mysql80-community-release-el7-7.noarch.rpm
     sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/mysql-community.repo
     yum --enablerepo=mysql80-community install mysql-community-server -y
   fi
-  echo "----------------------------------- MySQL installed -----------------------------------"
+  echo "*********************************** MySQL installed ***********************************"
 }
 
 install_redis() {
   # shellcheck disable=SC2046
   if ! [ $(command -v redis-cli) ]; then
-    echo "----------------------------------- Installing Redis -----------------------------------"
+    echo "*********************************** Installing Redis ***********************************"
     # Installed repo redis in PHP
     yum --enablerepo=remi install redis -y
   fi
-  echo "----------------------------------- Redis installed -----------------------------------"
+  echo "*********************************** Redis installed ***********************************"
 }
 
 enable_and_start_services() {
   yum update -y
-  echo "----------------------------------- Enabling and starting services -----------------------------------"
+  echo "*********************************** Enabling and starting services ***********************************"
   # shellcheck disable=SC2046
   if [ $(command -v systemctl) ]; then
-
-    if [ "$(systemctl is-active nginx)" = "active" ]; then
-      echo "Nginx web server is running."
-    else
-      echo "Nginx web server is not running."
+    if ! [ "$(systemctl is-active nginx)" = "active" ]; then
+      echo "*********************************** Starting Nginx ***********************************"
       systemctl enable --now nginx
     fi
-    systemctl enable --now php-fpm
-    systemctl enable --now mysqld
-    echo "----------------------------------- MySQL temporary password -----------------------------------"
-    grep "A temporary password" /var/log/mysqld.log
-    echo "You will need run this command to change password: mysql_secure_installation"
-    echo "------------------------------------------------------------------------------------------------"
-    systemctl enable --now redis
+    if ! [ "$(systemctl is-active php-fpm)" = "active" ]; then
+      echo "*********************************** Starting PHP-FPM ***********************************"
+      systemctl enable --now php-fpm
+    fi
+    if ! [ "$(systemctl is-active mysqld)" = "active" ]; then
+      echo "*********************************** Starting MySQL ***********************************"
+      systemctl enable --now mysqld
+    fi
+    if ! [ "$(systemctl is-active redis)" = "active" ]; then
+      echo "*********************************** Starting Redis ***********************************"
+      systemctl enable --now redis
+    fi
   fi
-  echo "----------------------------------- Services enabled and started -----------------------------------"
+  echo "*********************************** Services enabled and started ***********************************"
+  echo "*********************************** MySQL temporary password ***********************************"
+  grep "A temporary password" /var/log/mysqld.log
+  echo "You will need run this command to change password: mysql_secure_installation"
+  echo "************************************************************************************************"
 }
 
 main() {
@@ -117,10 +123,10 @@ main() {
       install_redis
       enable_and_start_services
     else
-      echo "- This script only support CentOS 7."
+      echo "* This script only support CentOS 7. *"
     fi
   else
-    echo "- This script only support CentOS 7."
+    echo "* This script only support CentOS 7. *"
   fi
 }
 
